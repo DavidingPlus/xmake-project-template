9f099db5faa6417190b995cb36a9dcca018f7357
DavidingPlus <davidingplus@qq.com>
2026-09-17 12:54:45 +0800

    release: v1.2.4

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 includes("config.lua")
 
 
-local version = "1.2.3"
+local version = "1.2.4"
 local export_headers_module = "export-headers"
 local export_headers_import_options = {rootdir = os.scriptdir(), anonymous = true}
 
